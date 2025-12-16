79b9e19e62a99e1d76c8c116470b50bbe21e7ce2
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-16 17:07:56 -0600

    creates API Service

---
+++ src/lib/Services/ApiService.ts
@@ -0,0 +1,20 @@
+namespace ApiService {
+	export function requestInit(
+		method: RequestInit['method'] = 'GET',
+		body?: RequestInit['body'],
+		credentials: RequestInit['credentials'] = 'include',
+	): RequestInit {
+		const headers = new Headers();
+
+		headers.set('Content-Type', 'application/json');
+
+		return {
+			credentials,
+			headers,
+			method,
+			body: body ? body : undefined,
+		};
+	}
+}
+
+export default ApiService;
