d44ac20e7de33e0b8b24d336944a633d5b93d750
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-16 17:09:32 -0600

    creates PublicNewsletter type and Newsletter Service with endpoint for public newsletters

---
+++ src/lib/Services/NewsletterService.ts
@@ -0,0 +1,12 @@
+import ApiService from '$lib/Services/ApiService';
+
+namespace NewsletterService {
+	export function getPublicNewslettersRequestInit() {
+		const url = `${import.meta.env.VITE_API_URL}/newsletters`;
+		const opts = ApiService.requestInit();
+
+		return { url, opts };
+	}
+}
+
+export default NewsletterService;
---
+++ src/lib/Types/NewsletterTypes.ts
@@ -0,0 +1,7 @@
+export type PublicNewsletterType = {
+	content: string;
+	description: string;
+	published_at: string;
+	slug: string;
+	title: string;
+};
