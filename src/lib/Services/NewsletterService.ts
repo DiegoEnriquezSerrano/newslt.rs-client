75acb0180b36bb4d573af0369b71fc9c84b20702
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-17 14:10:19 -0600

    Update prettier config and apply to project

--- src/lib/Services/NewsletterService.ts
+++ src/lib/Services/NewsletterService.ts
@@ -1,12 +1,12 @@
 import ApiService from '$lib/Services/ApiService';
 
 namespace NewsletterService {
-	export function getPublicNewslettersRequestInit() {
-		const url = `${import.meta.env.VITE_API_URL}/newsletters`;
-		const opts = ApiService.requestInit();
+  export function getPublicNewslettersRequestInit() {
+    const url = `${import.meta.env.VITE_API_URL}/newsletters`;
+    const opts = ApiService.requestInit();
 
-		return { url, opts };
-	}
+    return { url, opts };
+  }
 }
 
 export default NewsletterService;
--- src/lib/Types/NewsletterTypes.ts
+++ src/lib/Types/NewsletterTypes.ts
@@ -1,7 +1,7 @@
 export type PublicNewsletterType = {
-	content: string;
-	description: string;
-	published_at: string;
-	slug: string;
-	title: string;
+  content: string;
+  description: string;
+  published_at: string;
+  slug: string;
+  title: string;
 };
