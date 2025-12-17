0ff026dc9541c9f975a7e0b36a55924d61be3018
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-17 17:31:25 -0600

    Creates base layout wrapper component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>Hello, world!</h1>
+<script>
+  import PublicPageWrapper from '$lib/Components/Layouts/PublicPageWrapper.svelte';
+</script>
+
+<PublicPageWrapper>
+  <h1>Hello, world!</h1>
+</PublicPageWrapper>
