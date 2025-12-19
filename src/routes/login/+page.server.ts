d571ababfacd62f8aaf00f421da1cb4db600208e
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-19 17:57:10 -0600

    Updates CI to run full linter, also fixes lint errors

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,9 +2,9 @@ import { redirect } from '@sveltejs/kit';
 import AuthenticationService from '$lib/Services/AuthenticationService';
 
 export async function load({ fetch }) {
-  const userResponse = await AuthenticationService.Api.getAuthenticate(fetch);
+  const authenticationResponse = await AuthenticationService.Api.getAuthenticate(fetch);
 
-  if (userResponse.ok) {
+  if (authenticationResponse.ok) {
     throw redirect(307, '/home');
   }
 }
