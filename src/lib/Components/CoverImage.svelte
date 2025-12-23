13617270cc2e1c6f32577c9fe0b7b55fff4c862d
Diego Enriquez-Serrano <diego@diegoenriquezserrano.dev>
2025-12-23 12:34:15 -0600

    Adds cover image component and uses in newsletter issue card

---
+++ src/lib/Components/CoverImage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let { src = '', alt = '' }: { src: string; alt: string } = $props();
+</script>
+
+{#if Boolean(src.trim())}
+  <figure
+    class="flex-row border-style-outset border-color-gray border-width-2 border-rounded-8 overflow-hidden"
+  >
+    <img class="aspect-ratio-hd full-width" {src} {alt} />
+  </figure>
+{/if}
